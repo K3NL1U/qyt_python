d0fdc79f0a2074fe02631a7ef2a1930852fac306
Ken <kenliu.liu06@gmail.com>
2019-06-27 11:20:05 +1000

    Initial commit

---
+++ Chapter_3/Random_IPv4_Address.py
@@ -0,0 +1,13 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+import random
+sec1 = random.randint(1,255)
+sec2 = random.randint(1,255)
+sec3 = random.randint(1,255)
+sec4 = random.randint(1,255)
+
+ipadd = str(sec1) + '.' + str(sec2) + '.' + str(sec3) + '.' + str(sec4)
+
+print(ipadd)
