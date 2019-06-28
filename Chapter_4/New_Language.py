fb3c91d45d8fd71f0f569109086d0792ec62ae19
Ken <kenliu.liu06@gmail.com>
2019-06-28 15:28:54 +1000

    Initial commit

---
+++ Chapter_4/New_Language.py
@@ -0,0 +1,8 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+user_input = input('Please input a normal word: ')
+
+new_input = user_input[1:] + '-' + user_input[:1] + 'y'
+print(new_input)
