5ddf2abfba8260954b79986cd03908fb1dcd55c5
Ken <kenliu.liu06@gmail.com>
2019-06-29 18:18:00 +1000

    Initial commit

---
+++ Chapter_5/Comparing_Dictionary.py
@@ -0,0 +1,8 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+dict_a = {'python': 'reptile', 'bison': 'mammal', 'lion': 'mammal'}
+dict_b = {'python': 'reptile', 'lion': 'mammal', 'bison': 'mammal'}
+print(dict_a == dict_b)
+# 字典没有顺序，只要key和value有，而且对应就是正确的
