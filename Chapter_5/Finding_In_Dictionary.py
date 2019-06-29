b99339cb6327244adb56b2670265cbc4e41ac116
Ken <kenliu.liu06@gmail.com>
2019-06-29 19:57:27 +1000

    Initial commit

---
+++ Chapter_5/Finding_In_Dictionary.py
@@ -0,0 +1,9 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+performances = {'weekdays': ['Bearded Lady', 'Tiniest Man', 'Ventriloquist Vinnie'],\
+                'saturday': ['Amazing Acrobatics', 'Enchanted Elephants'],\
+                'sunday': ['Snake Charmer', 'Amazing Acrobatics']}
+
+print(performances.get('Ventriloquist Vinnie'))
---
+++ Chapter_5/Finding_In_Matrix.py
@@ -0,0 +1,13 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+# weekdays = ['Bearded Lady', 'Tiniest Man', 'Ventriloquist Vinnie']
+# saturday = ['Amazing Acrobatics', 'Enchanted Elephants']
+# sunday = ['Snake Charmer', 'Amazing Acrobatics']
+
+performances = [['Bearded Lady', 'Tiniest Man', 'Ventriloquist Vinnie'],\
+                ['Amazing Acrobatics', 'Enchanted Elephants'],\
+                ['Snake Charmer', 'Amazing Acrobatics']]
+
+print(performances[0][2])
