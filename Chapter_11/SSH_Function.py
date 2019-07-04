cc253bfe63bda48c60f705b5b62f0337ba7a1fad
Ken <kenliu.liu06@gmail.com>
2019-07-04 19:12:59 +1000

    Initial commit

--- Chapter_11/SSH_Function.py
+++ Chapter_11/SSH_Function.py
@@ -4,4 +4,16 @@
 
 import paramiko
 
+def qytang_ssh(ip, username, password, port=22, cmd='1s'):
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, port=port, username=username, password=password, timeout=5, compress=True)
+    stdin, stdout, stderr = ssh.exec_command('cmd')
+    x = stdout.read().decode()
+    return x
 
+
+if __name__ == '__main__':
+    print(qytang_ssh('10.0.0.31', 'root', 'Cisc0123'))
+    print(qytang_ssh('10.0.0.31', 'root', 'Cisc0123', cmd='pwd'))
