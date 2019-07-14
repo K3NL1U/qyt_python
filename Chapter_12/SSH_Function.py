a24ca02e1417b569749dde41a89fc5ce2abab71c
Ken <kenliu.liu06@gmail.com>
2019-07-14 18:09:13 +1000

    Initial commit

---
+++ Chapter_12/SSH_Function.py
@@ -0,0 +1,20 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+import paramiko
+
+
+def ssh(ip, username, password, port=22, cmd='ls'):
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, port=port, username=username, password=password, timeout=5, compress=True)
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    x = stdout.read().decode()
+    return x
+
+
+if __name__ == '__main__':
+    # print(ssh('10.0.0.34', 'ken', 'Cisc0123'))
+    print(ssh('10.0.0.34', 'ken', 'Cisc0123', cmd='pwd'))  # pwd is present working directory
