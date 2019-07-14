e1fa46f8ce636e460b144f439c578ece5a871dfe
Ken <kenliu.liu06@gmail.com>
2019-07-14 18:25:34 +1000

    Initial commit

--- Chapter_12/Module_ping_ssh.py
+++ Chapter_12/Module_ping_ssh.py
@@ -3,7 +3,8 @@
 
 
 from Ping_Function import ping
-from SSH_Function import qytang_ssh
+from SSH_Function import ssh
+
 
 def ping_and_ssh(ip_list, username, password, cmd):
     for ip in ip_list:
@@ -11,13 +12,13 @@ def ping_and_ssh(ip_list, username, password, cmd):
         if ping_result[1] == 1:
             print(ping_result[0], '可达！')
             print('登录', ping_result[0], '执行命令', cmd, '回显结果如下：')
-            print(qytang_ssh(ping_result[0], username=username, password=password, cmd=cmd))
+            print(ssh(ping_result[0], username=username, password=password, cmd=cmd))
         else:
             print(ping_result[0], '不可达！')
 
 
 if __name__ == '__main__':
-    ip_list = ['10.0.0.31']
-    username = 'root'
+    ip_list = ['10.0.0.34', '192.168.69.169']
+    username = 'ken'
     password = 'Cisc0123'
     ping_and_ssh(ip_list, username, password, 'ls')
