7724e791e5b2c2a9786e7b54130659675b16e923
Ken <kenliu.liu06@gmail.com>
2019-07-18 21:32:36 +1000

    Initial commit

---
+++ Chapter_14/Platform_test.py
@@ -0,0 +1,12 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+import sys
+
+if 'win' in sys.platform:
+    print('this is windows!')
+elif 'linux' in sys.platform:
+    print('this is linux!')
+else:
+    print('other system')
