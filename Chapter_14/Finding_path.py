fc76defb6015d38f556bbc3b2b1c3bca867e0cc1
Ken <kenliu.liu06@gmail.com>
2019-07-18 21:44:05 +1000

    Initial commit

---
+++ Chapter_14/Finding_path.py
@@ -0,0 +1,10 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+import sys
+
+# sys.path.insert(1, '/root')  # for linux
+# sys.path.insert(1, 'C:\\')  # for windows
+
+print(sys.path)
