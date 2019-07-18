cb485fe890b68c9b5f7015615adba5b6f826157e
Ken <kenliu.liu06@gmail.com>
2019-07-18 17:20:57 +1000

    Initial commit

--- Chapter_13/Changing_to_Class.py
+++ Chapter_13/Changing_to_Class.py
@@ -43,3 +43,7 @@ class QYT_PING:
         else:
             return '<srcip: {0}, dstip: {1}, size: {2}>'.format(self.srcip, self.ip, self.length)
 
+if __name__ == '__main__':
+    ping = QYT_PING('10.0.0.34')
+    ping.one()
+    ping.ping()
