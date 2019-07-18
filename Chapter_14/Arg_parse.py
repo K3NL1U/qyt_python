e8b5f28387de3ee39478aa504be1bfbe7bba901c
Ken <kenliu.liu06@gmail.com>
2019-07-18 22:36:18 +1000

    Initial commit

--- Chapter_14/Arg_parse.py
+++ Chapter_14/Arg_parse.py
@@ -2,22 +2,21 @@
 # https://gitee.com/K3NL1U
 
 
-def arg_parse(host, filename, interface):
-    print(host)
-    print(filename)
-    print(interface)
+from SSH_Function import ssh
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
-    usage = 'usage: python3 Arg_parse.py ipaddress -f filename -i interface'
+    usage = 'usage: python3 SSH_Function.py -i ipaddr -u username -p password -c command'
 
     parser = ArgumentParser(usage=usage)
 
-    parser.add_argument('-f', '--file', dest='filename', help='Write content to FILE', default='1.txt', type=str)
-    parser.add_argument('-i', '--iface', dest='interface', help='Specify an interface', default=1, type=int)
-    parser.add_argument(nargs='?', dest='host', help='Specify an host', default='10.1.1.1', type=str)
+    parser.add_argument('-i', '--ipaddr', dest='ipaddr', help='SSH Server', default='10.0.0.34', type=str)
+    parser.add_argument('-u', '--username', dest='username', help='SSH Username', default='ken', type=str)
+    parser.add_argument('-p', '--password', dest='password', help='SSH Password', default='Cisc0123', type=str)
+    parser.add_argument('-c', '--command', dest='command', help='Shell Command', default='ls', type=str)
+
     args = parser.parse_args()
 
-    arg_parse(args.host, args.filename, args.interface)
+    print(ssh(args.ipaddr, args.username, args.password, cmd=args.command))
