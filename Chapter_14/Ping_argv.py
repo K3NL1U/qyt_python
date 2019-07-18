68ae4a30970f216134118e3cc3302e91ef7c1e1d
Ken <kenliu.liu06@gmail.com>
2019-07-18 21:52:00 +1000

    Initial commit

---
+++ Chapter_14/Ping_argv.py
@@ -0,0 +1,24 @@
+# https://github.com/K3NL1U
+# https://gitee.com/K3NL1U
+
+
+import logging
+logging.getLogger('kamene,runtime').setLevel(logging.ERROR)
+from kamene.all import *
+
+def ping(ip):
+    ping_pkt = IP(dst=ip) / ICMP()
+    ping_result = sr1(ping_pkt, timeout=2, verbose=False)
+    if ping_result:
+        return ip, 1
+    else:
+        return ip, 0
+
+if __name__ == '__main__':
+    import sys
+    result = ping(sys.argv[1])
+
+    if result[1]:
+        print(result[0], '通！')
+    else:
+        print(result[0], '不通！')
